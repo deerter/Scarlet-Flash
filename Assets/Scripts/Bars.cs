107fec60d4ca54ae020698705ef642b8e1ba77a5
deerter <alvarobarrios94@hotmail.es>
2020-08-20 21:31:54 +0200

    Red health + Hyper bar development

--- Assets/Scripts/Bars.cs
+++ Assets/Scripts/Bars.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Bars : MonoBehaviour
 {
@@ -9,7 +10,9 @@ public class Bars : MonoBehaviour
     [SerializeField] GameObject hyper;
     [SerializeField] GameObject special;
     static int maxHealth = 1000000;
+    static int maxHyper = 1000000;
     int currentHealth = maxHealth;
+    int currentRedHealth = maxHealth;
     static int maxSpecialTime = 5000;
     int currentSpecialTime = maxSpecialTime;
     Coroutine coroutineSpecial;
@@ -22,9 +25,15 @@ public class Bars : MonoBehaviour
         special.transform.localScale = new Vector3(0.15f, 1f);
     }*/
 
+
     public void SetHP(float hpNormalized)
     {
-        health.transform.GetChild(1).localScale = new Vector3(hpNormalized, 1f);
+        health.transform.GetChild(2).localScale = new Vector3(hpNormalized, 1f);
+    }
+
+    public void SetRedHealth(float redHealthNormalized)
+    {
+        health.transform.GetChild(1).localScale = new Vector3(redHealthNormalized, 1f);
     }
 
     public void SetHyper(float hyperNormalized)
@@ -40,13 +49,34 @@ public class Bars : MonoBehaviour
     public bool TakeDamage(int attackValue)
     {
         currentHealth -= attackValue / 2;  //Half is red health
+        currentRedHealth -= attackValue / 4;
+        
+
         if (currentHealth <= 0)
         {
-            currentHealth = 0;
-            SetHP((float)currentHealth / maxHealth);
+            SetHP(0);
+            SetRedHealth(0);
             return true;
         }
         SetHP((float)currentHealth / maxHealth);
+        SetRedHealth((float)currentRedHealth / maxHealth);
+        return false;
+    }
+
+    public bool increaseHyperBar(int attackValue)
+    {
+        //var newHyperBar = Instantiate(hyper, hyper.transform.position, hyper.transform.rotation, hyper.transform);
+        GameObject newHyperBar = Instantiate(hyper.transform.GetChild(1).gameObject);
+        newHyperBar.transform.SetParent(hyper.transform);
+        newHyperBar.transform.localScale = hyper.transform.GetChild(1).localScale;
+        newHyperBar.transform.localPosition = hyper.transform.GetChild(1).localPosition;
+
+        newHyperBar.name = "HyperBarFullLevel2";
+        //newHyperBar.transform.position = new Vector3(newHyperBar.transform.position.x,newHyperBar.transform.position.y + 50,newHyperBar.transform.position.z);
+        //print(newHyperBar.transform.position);
+        //newHyperBar.GetComponent<Image>().color = new Color(31, 186, 208);
+
+
         return false;
     }
 
@@ -69,6 +99,7 @@ public class Bars : MonoBehaviour
         if (Input.GetKeyDown(GameConstants.LP))
         {
             TakeDamage(45000);
+            increaseHyperBar(45000);
         }
 
         if (Input.GetKeyDown(GameConstants.HP))
