2d443062f648ce070406bad7f55d64739bb9f28b
deerter <alvarobarrios94@hotmail.es>
2021-03-22 20:01:55 +0100

    ken animation events and cropped ryu spritesheet

--- Assets/Scripts/Characters/Ken/Ken.cs
+++ Assets/Scripts/Characters/Ken/Ken.cs
@@ -6,20 +6,35 @@ public class Ken : Character {
 
 	public Ken() : base(){
 		name = "ken";
-		/*health = 850000;
+		health =1000000;
 		doubleJumpCount = 0;
 		airDashCount = 0;
-    	lightPunchDamage = 40000;
+    	lightPunchDamage = 45000;
     	lightKickDamage = 35000;
-    	heavyPunchDamage = 75000;
-    	heavyKickDamage = 75000;
+    	heavyPunchDamage = 80000;
+    	heavyKickDamage = 80000;
     	crouchingLightPunchDamage = 40000;
     	crouchingLightKickDamage = 40000;
-    	crouchingHeavyPunchDamage = 75000;
-    	crouchingHeavyKickDamage = 80000;
+    	crouchingHeavyPunchDamage = 70000;
+    	crouchingHeavyKickDamage = 75000;
     	jumpingLightPunchDamage = 40000;
     	jumpingLightKickDamage = 40000;
     	jumpingHeavyPunchDamage = 80000;
-    	jumpingHeavyKickDamage = 75000;*/
+    	jumpingHeavyKickDamage = 75000;
+
+		attackOutput = new Dictionary<string, int> {
+			{AnimationStates.LIGHT_PUNCH, this.lightPunchDamage},
+			{AnimationStates.LIGHT_KICK, this.lightKickDamage},
+			{AnimationStates.HEAVY_PUNCH, this.heavyPunchDamage},
+			{AnimationStates.HEAVY_KICK, this.heavyKickDamage},
+			{AnimationStates.CROUCHING_LIGHT_PUNCH, this.crouchingLightPunchDamage},
+			{AnimationStates.CROUCHING_LIGHT_KICK, this.crouchingLightKickDamage},
+			{AnimationStates.CROUCHING_HEAVY_PUNCH, this.crouchingHeavyPunchDamage},
+			{AnimationStates.CROUCHING_HEAVY_KICK, this.crouchingHeavyKickDamage},
+			{AnimationStates.JUMPING_LIGHT_PUNCH, this.jumpingLightPunchDamage},
+			{AnimationStates.JUMPING_LIGHT_KICK, this.jumpingLightKickDamage},
+			{AnimationStates.JUMPING_HEAVY_PUNCH, this.jumpingHeavyPunchDamage},
+			{AnimationStates.JUMPING_HEAVY_KICK, this.jumpingHeavyKickDamage}
+		};
 	}
 }
