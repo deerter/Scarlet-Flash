f175444cb3702741f5e845463ee5eda8a30cac27
deerter <alvarobarrios94@hotmail.es>
2021-03-23 20:20:43 +0100

    ryu sprites and swap characters controller

---
+++ Assets/Scripts/Characters/CharacterAssist.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAssist : MonoBehaviour {
+
+	private bool swapped;  //Used to check if the player swapped their characters recently.
+	private CharacterFeatures currentCharacter;
+
+	// Use this for initialization
+	void Start () {
+		swapped = false;
+		currentCharacter = this.GetComponent<CharacterFeatures>();
+	}
+
+	IEnumerator SwappingTimeout(){
+		swapped = true;
+		yield return new WaitForSeconds(5);
+		swapped = false;
+	}
+
+	private void Swap(){
+		if (SwapAvailable()){
+			StartCoroutine(SwappingTimeout());
+		}
+	}
+
+	private bool SwapAvailable(){
+		return (swapped==false && !currentCharacter.GetIsJumping() && !currentCharacter.GetIsCrouching() && !currentCharacter.IsAnimationPlaying());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(GameConstants.A1)){  ///Swap for first assist character
+			/////Executes only one command, one function checks if the swap is available and performs it if so
+			Swap();
+			
+		}
+
+		if (Input.GetKeyDown(GameConstants.A2)){  ///Swap for second assist character
+			////Executes only one command
+			Swap();
+		}
+		
+	}
+}
--- Assets/Scripts/Characters/CharacterMovement.cs
+++ Assets/Scripts/Characters/CharacterMovement.cs
@@ -64,7 +64,6 @@ public class CharacterMovement : MonoBehaviour {
 						break;
 		}
 			
-		print(characterJumpFinalPosition);
 		
 	}
 
