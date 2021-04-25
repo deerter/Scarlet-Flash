5ccba1f86d262e89a0653dc9e006d17fdeeea565
deerter <alvarobarrios94@hotmail.es>
2021-04-25 20:08:28 +0200

    Fight settings arrows

--- Assets/Scripts/Scenes/ChooseSetting.cs
+++ Assets/Scripts/Scenes/ChooseSetting.cs
@@ -7,11 +7,19 @@ using UnityEngine.UI;
 public class ChooseSetting : MonoBehaviour {
 	[SerializeField] private string[] settingOptions;
 	[SerializeField] private GameObject settingText;
+	[SerializeField] private GameObject leftArrow;
+	[SerializeField] private GameObject rightArrow;
 	[SerializeField] private int optionSelected;
+
+	private void SetArrows(){
+		rightArrow.transform.localPosition = new Vector2(settingText.transform.localPosition.x + (settingText.GetComponent<Text>().preferredWidth/2) + 30, rightArrow.transform.localPosition.y);
+		leftArrow.transform.localPosition = new Vector2(settingText.transform.localPosition.x - (settingText.GetComponent<Text>().preferredWidth/2) - 30, rightArrow.transform.localPosition.y);
+	}
 
 	// Use this for initialization
 	void Start () {
-		
+		//Sets the position of both arrows//
+		SetArrows();
 	}
 	
 	// Update is called once per frame
@@ -21,15 +29,19 @@ public class ChooseSetting : MonoBehaviour {
 				optionSelected--;
 				settingText.GetComponent<Texts>().SetText(settingOptions[optionSelected]);
 				if (optionSelected==0){
-					///Disable left arrow
+					leftArrow.SetActive(false);
 				}
+				rightArrow.SetActive(true);
+				SetArrows();
 			}
 			if (Input.GetKeyDown(GameConstants.R) && (optionSelected != settingOptions.Length-1)){
 				optionSelected++;
 				settingText.GetComponent<Texts>().SetText(settingOptions[optionSelected]);
-				if (optionSelected==settingOptions.Length+1){
-					///Disable right arrow
+				if (optionSelected==settingOptions.Length-1){
+					rightArrow.SetActive(false);
 				}
+				leftArrow.SetActive(true);
+				SetArrows();
 			}
 		}
 		
